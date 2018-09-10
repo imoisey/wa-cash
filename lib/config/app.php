26f83257114626388875fed8e7133b3885c3fd52
Igor Moiseev <developer.modx@gmail.com>
2018-09-10 09:39:08 +0300

    Config File

--- lib/config/app.php
+++ lib/config/app.php
@@ -4,9 +4,5 @@ return array (
   'img'	=> 'img/cash96.png',
   'version' => '1.0.0',
   'vendor' => 'local',
-<<<<<<< HEAD
-=======
-  'frontend' => true,
->>>>>>> 4295b67661653f63a8a93d89ac44a65d19dd31c1
   'rights' => 1,
 );
